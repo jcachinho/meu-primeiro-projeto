9e64246c26d6dde32a2efb948a828cd7bed14f9d
jcachinho <jcachinho.94@gmail.com>
2019-10-01 23:35:41 +0100

    Alteração na classe principal 2

--- src/main/java/com/empresa/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/empresa/api/MeuPrimeiroProjetoApplication.java
@@ -9,6 +9,7 @@ public class MeuPrimeiroProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Hello world");
+		System.out.println("AAAAA");
 	}
 
 }
